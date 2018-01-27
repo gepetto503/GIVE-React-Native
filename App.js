f0fc6d06ad9d11eb670a2ef5609f72071a998d53
Brendan Hanna <gepetto503@gmail.com>
2018-01-26 16:12:10 -0800

    got tab navigation up and running.  WOO HOO!

--- App.js
+++ App.js
@@ -1,18 +1,56 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TabNavigator } from 'react-navigation';
 
+//these import classes for each component
 import TitleBar from './TitleBar';
 import MainView from './MainView';
 import BottomNav from './BottomNav';
 
+
+class ProfileScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Profile Stuff</Text>
+      </View>
+    );
+  }
+}
+
+class SearchScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Search Stuff</Text>
+      </View>
+    );
+  }
+}
+
+class SocialScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Social Stuff</Text>
+      </View>
+    );
+  }
+}
+
+//creates a navigator
+const MainScreenNavigator = TabNavigator({
+  Profile: { screen: ProfileScreen },
+  Search: { screen: SearchScreen },
+  Social: { screen: SocialScreen }
+});
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <TitleBar />
-        <MainView />
-        <BottomNav />
+        <MainScreenNavigator />
       </View>
     );
   }
